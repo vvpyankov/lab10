ace09dd759f1b423a79db7ce24a7ae8d0f352acb
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-01-18 21:46:47 +0300

    18.01.2022

--- lab10zadacha/Program.cs
+++ lab10zadacha/Program.cs
@@ -13,22 +13,23 @@ namespace lab10zadacha
 			Angle angle = new Angle();
 			try
 			{
-				Console.WriteLine("Для перевода заначения угла в радианы введите значение угла. ");
-				Console.WriteLine("Целочисленное значение градуса угла = ");
+				Console.WriteLine("Укажите значение угла (целочисленные значения!!!)");
+				Console.Write("Градусы угла - ");
 				angle.Grad = Convert.ToInt32(Console.ReadLine());
 
-				Console.WriteLine("Минуты угла: ");
+				Console.Write("Минуты угла - ");
 				angle.Min = Convert.ToInt32(Console.ReadLine());
 
-				Console.WriteLine("Секунды угла: ");
+				Console.Write("Секунды угла - ");
 				angle.Sec = Convert.ToInt32(Console.ReadLine());
 			}
 			catch (FormatException)
 			{
-				Console.WriteLine("Неверный формат ввода значения.");
+				Console.WriteLine("Формат значений - некорректен!");
 				return;
 			}
-			angle.GetToRadians();
+			angle.ToRadians();
+			Console.ReadKey();
 		}
 	}
 	class Angle
@@ -47,7 +48,7 @@ namespace lab10zadacha
 				}
 				else
 				{
-					Console.WriteLine("Введите целочисленное значение угла в градусах в диапазоне [0,180");
+					Console.WriteLine("Это не целое число от 0 до 180");
 				}
 			}
 			get
@@ -62,11 +63,11 @@ namespace lab10zadacha
 			{
 				if (value >= 0 && value <= 60)
 				{
-					Min = value;
+					min = value;
 				}
 				else
 				{
-					Console.WriteLine("Введите целочисленное значение минут углах  в диапазоне от [0,60]");
+					Console.WriteLine("Это не целое число от 0 до 60");
 				}
 			}
 			get
@@ -84,7 +85,7 @@ namespace lab10zadacha
 				}
 				else
 				{
-					Console.WriteLine("Введите целочисленное значение угла в минутах в диапазоне [0;,60]");
+					Console.WriteLine("Это не целое число от 0 до 60");
 				}
 			}
 			get
@@ -100,10 +101,10 @@ namespace lab10zadacha
 			Sec = sec;
 		}
 
-		public void GetToRadians()
+		public void ToRadians()
 		{
-			double Radian = (grad + (min + (double)sec / 60) / 60) * Math.PI / 180;
-			Console.WriteLine("{0:f7}", Radian);
+			double Radian = (grad + (min + sec / 60) / 60) * Math.PI / 180;
+			Console.WriteLine("Угол в радианах - {0:f2}", Radian);
 		}
     }
 }
